d579f64f5ed64da5c16c3f22cdaf817172897737
MarcWebEquip <marc.morelle@web-equip.fr>
2016-09-22 12:08:03 +0200

    Ajouté mon index.php YaHoooooogit log! #Ticket Mantis 1

---
+++ index.php
@@ -0,0 +1,327 @@
+<?PHP
+/* */ 
+include "header.php";
+?>
+
+ <!-- Portfolio Grid Section -->
+    <section id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Portfolio</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/cabin.png" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/cake.png" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/circus.png" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal4" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/game.png" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal5" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/safe.png" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal6" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/submarine.png" class="img-responsive" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+	
+	<!-- PortFolio 2 >> Goals -->
+	
+	<!-- Portfolio Grid Section -->
+    <section id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Personnal Goals</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+			
+			
+		<div id="wrapper">
+			<section id="list">
+				<div class="year">
+				
+				<h3>2017</h3>
+					<ul class="items">
+						<li class="event">
+							<span class="glyphicon glyphicon-signal"></span><span class="name">
+							<a href="#"><b>Certification Google Adwords / Google Shopping</b></a></span>
+							<span class="role">Attendee</span><span class="state stalled">En cours</span>
+							<span class="description" style="display: block;">
+<ul>
+<li>Bases de la publicité – 90 questions – Note d’admission : 85 % ;</li>
+<li>Examen avancé – Publicité sur le réseau de recherche – 99 questions – Note d’admission : 80 % ;</li>
+<li>Examen avancé – Publicité display – 90 questions – Note d’admission : 70 %.</li>
+</ul>
+							<a class="readmore" target="_blank" href="https://www.facebook.com/media/set/?set=a.10151562949920272.1073741825.191921000271&amp;type=1">Read more »</a>
+							</span>
+						</li>
+						
+						<li class="event">
+							<span class="glyphicon glyphicon-signal"></span><span class="name">
+							<a href="#"><b>Certification Google Analitics</b></a></span>
+							<span class="role">Attendee</span><span class="state stalled">En cours</span>
+							<span class="description" style="display: block;">
+<ul>
+<li>1</li>
+<li>2</li>
+<li>3</li>
+</ul>
+							<a class="readmore" target="_blank" href="https://www.facebook.com/media/set/?set=a.10151562949920272.1073741825.191921000271&amp;type=1">Read more »</a>
+							</span>
+						</li>
+						
+						<li class="event">
+							<span class="glyphicon glyphicon-signal"></span><span class="name">
+							<a href="#">Certification Symfony</a></span>
+							<span class="role">Attendee</span><span class="state completed">Completed</span>
+							<span class="description" style="display: block;">Our team was picked to attended the HackEd 2.0 Hackathon at Facebook HQ in Menlo Park, CA. Competing against roughly 25 other teams to build educational apps, especailly focused on low-income families.
+							<a class="readmore" target="_blank" href="https://www.facebook.com/media/set/?set=a.10151562949920272.1073741825.191921000271&amp;type=1">Read more »</a>
+							</span>
+						</li>
+						
+						<li class="event">
+							<span class="fa-facebook-official"></span><span class="name">
+							<a href="#">Certification Symfony</a></span>
+							<span class="role">Attendee</span><span class="state completed">Completed</span>
+							<span class="description" style="display: block;">Our team was picked to attended the HackEd 2.0 Hackathon at Facebook HQ in Menlo Park, CA. Competing against roughly 25 other teams to build educational apps, especailly focused on low-income families.
+							<a class="readmore" target="_blank" href="https://www.facebook.com/media/set/?set=a.10151562949920272.1073741825.191921000271&amp;type=1">Read more »</a>
+							</span>
+						</li>
+					</ul>
+					
+					
+					<h3>2016</h3>
+					<ul class="items">
+					
+						<li class="event">
+							<span class="glyphicon glyphicon-wrench"></span><span class="name">
+							<a href="#"><b>Auto Formation Vagrant</b></a></span>
+							<span class="role">Attendee</span><span class="state completed">Completed</span>
+							<span class="description" style="display: block;">Our team was picked to attended the HackEd 2.0 Hackathon at Facebook HQ in Menlo Park, CA. Competing against roughly 25 other teams to build educational apps, especailly focused on low-income families.
+							<a class="readmore" target="_blank" href="https://www.facebook.com/media/set/?set=a.10151562949920272.1073741825.191921000271&amp;type=1">Read more »</a>
+							</span>
+						</li>
+						
+						<li class="event">
+							<span class="fa fa-git"></span><span class="name">
+							<a href="#"><b>Auto Formation Vagrant</b></a></span>
+							<span class="role">Attendee</span><span class="state completed">Completed</span>
+							<span class="description" style="display: block;">Our team was picked to attended the HackEd 2.0 Hackathon at Facebook HQ in Menlo Park, CA. Competing against roughly 25 other teams to build educational apps, especailly focused on low-income families.
+							<a class="readmore" target="_blank" href="https://www.facebook.com/media/set/?set=a.10151562949920272.1073741825.191921000271&amp;type=1">Read more »</a>
+							</span>
+						</li>
+					
+					
+						<li class="event">
+							<span class="glyphicon glyphicon-shopping-cart"></span><span class="name">
+							<a href="#"><b>Formation Prestashop Développeur Niveau 1</b></a></span>
+							<span class="role">Attendee</span><span class="state completed">Completed</span>
+							<span class="description" style="display: block;">Our team was picked to attended the HackEd 2.0 Hackathon at Facebook HQ in Menlo Park, CA. Competing against roughly 25 other teams to build educational apps, especailly focused on low-income families.
+							<a class="readmore" target="_blank" href="https://www.facebook.com/media/set/?set=a.10151562949920272.1073741825.191921000271&amp;type=1">Read more »</a>
+							</span>
+						</li>
+						
+						<li class="event">
+							<span class="glyphicon glyphicon-shopping-cart"></span><span class="name">
+							<a href="#"><b>Formation Prestashop Développeur de Modules</b></a></span>
+							<span class="role">Attendee</span><span class="state ongoing">A l'étude</span>
+							<span class="description" style="display: block;">Our team was picked to attended the HackEd 2.0 Hackathon at Facebook HQ in Menlo Park, CA. Competing against roughly 25 other teams to build educational apps, especailly focused on low-income families.
+							<a class="readmore" target="_blank" href="https://www.facebook.com/media/set/?set=a.10151562949920272.1073741825.191921000271&amp;type=1">Read more »</a>
+							</span>
+						</li>
+					</ul>
+					
+					<h3>2015</h3>
+					<ul class="items">
+						<li class="event">
+							<span class="fa fa-html5" aria-hidden="true""></span>
+							<span class="name"><a href="#"><b>Certification Html5</b></a></span>
+							<span class="role">Attendee</span><span class="state stalled">En cours</span>
+							<span class="description" style="display: block;">Our team was picked to attended the HackEd 2.0 Hackathon at Facebook HQ in Menlo Park, CA. Competing against roughly 25 other teams to build educational apps, especailly focused on low-income families.
+							<a class="readmore" target="_blank" href="https://www.facebook.com/media/set/?set=a.10151562949920272.1073741825.191921000271&amp;type=1">Read more »</a>
+							</span>
+						</li>
+						
+						<li class="event">
+							<span class="fa fa-css3"></span><span class="name">
+							<a href="#">Certification Css3</a></span>
+							<span class="role">Attendee</span><span class="state completed">Completed</span>
+							<span class="description" style="display: block;">Our team was picked to attended the HackEd 2.0 Hackathon at Facebook HQ in Menlo Park, CA. Competing against roughly 25 other teams to build educational apps, especailly focused on low-income families.
+							<a class="readmore" target="_blank" href="https://www.facebook.com/media/set/?set=a.10151562949920272.1073741825.191921000271&amp;type=1">Read more »</a>
+							</span>
+						</li>
+						
+						<li class="event">
+							<span class="fa fa-google-plus"></span><span class="name">
+							<a href="#">Chaine Google plus</a></span>
+							<span class="role">Attendee</span><span class="state completed">Completed</span>
+							<span class="description" style="display: block;">Our team was picked to attended the HackEd 2.0 Hackathon at Facebook HQ in Menlo Park, CA. Competing against roughly 25 other teams to build educational apps, especailly focused on low-income families.
+							<a class="readmore" target="_blank" href="https://www.facebook.com/media/set/?set=a.10151562949920272.1073741825.191921000271&amp;type=1">Read more »</a>
+							</span>
+						</li>
+						
+						<li class="event">
+							<span class="fa fa-youtube"></span><span class="name">
+							<a href="#">Chaine Youtube</a></span>
+							<span class="role">Attendee</span><span class="state completed">Completed</span>
+							<span class="description" style="display: block;">Our team was picked to attended the HackEd 2.0 Hackathon at Facebook HQ in Menlo Park, CA. Competing against roughly 25 other teams to build educational apps, especailly focused on low-income families.
+							<a class="readmore" target="_blank" href="https://www.facebook.com/media/set/?set=a.10151562949920272.1073741825.191921000271&amp;type=1">Read more »</a>
+							</span>
+						</li>
+						
+						<li class="event">
+							<span class="fa fa-wordpress"></span><span class="name">
+							<a href="#">Création de Blogs sous Wordpress </a></span>
+							<span class="role">Attendee</span><span class="state completed">Completed</span>
+							<span class="description" style="display: block;">Our team was picked to attended the HackEd 2.0 Hackathon at Facebook HQ in Menlo Park, CA. Competing against roughly 25 other teams to build educational apps, especailly focused on low-income families.
+							<a class="readmore" target="_blank" href="https://www.facebook.com/media/set/?set=a.10151562949920272.1073741825.191921000271&amp;type=1">Read more »</a>
+							</span>
+						</li>
+					</ul>
+				</div>
+			</section>		
+           </div> 
+		   </div>
+           
+        </div>
+    </section>
+	
+	<!-- Fin Goals -->
+
+    <!-- About Section -->
+    <section class="success" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>About</h2>
+                    <hr class="star-light">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <p>Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.</p>
+                </div>
+                <div class="col-lg-4">
+                    <p>Whether you're a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!</p>
+                </div>
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <a href="#" class="btn btn-lg btn-outline">
+                        <i class="fa fa-download"></i> Download Theme
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Contact Me</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
+                    <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
+                    <form name="sentMessage" id="contactForm" novalidate>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Name</label>
+                                <input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Email Address</label>
+                                <input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Phone Number</label>
+                                <input type="tel" class="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Message</label>
+                                <textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <br>
+                        <div id="success"></div>
+                        <div class="row">
+                            <div class="form-group col-xs-12">
+                                <button type="submit" class="btn btn-success btn-lg">Send</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+<?PHP 
+/* */ 
+include "footer.php";
+?>
